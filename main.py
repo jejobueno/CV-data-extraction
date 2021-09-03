eb7252fa5718c0aca894485ae3c0c601c714c68a
jessbuenos@gmail.com <Brussel.20>
2021-09-03 12:01:34 +0200

    'fixing sectioning'

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from utils.extractSections import get_section_data
+from tika import parser
+# opening pdf file
+parsed_pdf = parser.from_file('data/pdf/3.pdf')
+# saving content of pdf
+# you can also bring text only, by parsed_pdf['text']
+# parsed_pdf['content'] returns string
+data = parsed_pdf['content']
 
-sections = get_section_data('data/word/2.docx')
+sections = get_section_data(data)
+
+for section in sections:
+    print(section)
+    print(sections[section])
 
